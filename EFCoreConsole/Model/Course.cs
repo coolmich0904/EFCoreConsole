dd4f6ccf18b0579f21a37ba9ee9d18667c472d07
Irene Lee <69160325+coolirene@users.noreply.github.com>
2021-07-04 19:39:51 -0400

    Add project files.

---
+++ EFCoreConsole/Model/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreConsole.Model
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public Nullable<int> TeacherId { get; set; }
+        public virtual Teacher Teacher { get; set; }
+
+        public DateTime? CourseDate { get; set; }
+        // public ICollection<Student> Students { get; set; }
+    }
+}
---
+++ EFCoreConsole/Program.cs
@@ -0,0 +1,95 @@
+﻿/*
+NOTE: To install EF package, ref : https://www.entityframeworktutorial.net/efcore/install-entity-framework-core.aspx 
+
+For more information about how to use "dotnet ef..." Refer to ReadMe.txt
+*/
+
+using EFCoreConsole.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EFCoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.
+            // pre-requiste: go to EFCoreConsole\instnwnd.sql and copy and paste into SSMS to create database first
+            // DataBaseFirstApproach();
+
+            // 2. 
+             CodeFirstApporach();
+
+        }
+
+        private static void DataBaseFirstApproach()
+        {
+             var obj = new DatabaseFirst();
+            // 1. Add
+            // obj.AddData(5, "NorthEast");
+
+            // 2. Update
+            // obj.UpdateData(4, "NorthEast-Update");
+
+            // 3. Delete
+            // obj.DeleteData(5);
+
+            // 4. Query
+            //obj.GetData();
+
+            // 5. Query
+            //obj.GetData("Eastern");
+
+            // 6. Raw SQL
+            // obj.GetDataUsingRawSQL();
+
+            // 7. Execute SQL (INSERT, UPDATE and DELETE) using Raw SQL
+            // obj.ExecuteSqlCommand();
+
+            // 8. Transaction Support
+            // obj.TransactionSupport();
+
+        }
+
+        private static void CodeFirstApporach()
+        {
+            var obj = new CodeFirstApporach();
+
+            //// 1. Add
+            obj.AddData("Now");
+
+            //// 1-1 
+            var stdAddress = new StudentAddress()
+            {
+                Address1 = "1 King",
+                City = "Toronto",
+            };
+            obj.AddData("Hardy", stdAddress);
+
+            //// 2. Update
+            //obj.UpdateData(5, "NorthEast-Update");
+
+            //// 3. Delete
+            // obj.DeleteData(1);
+
+            //// 4. Query
+            //  obj.GetData();
+
+            //// 5. Query
+            //obj.GetData("Estern");
+
+            //// 6. Raw SQL (SELECT)
+            //obj.GetDataUsingRawSQL();
+
+            //// 7. Execute SQL (INSERT, UPDATE and DELETE) using Raw SQL
+            //obj.ExecuteSqlCommand();
+
+            //// 8. Transaction Support
+            //obj.TransactionSupport();
+        }
+    }
+}
